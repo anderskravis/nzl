b74753c214d9d68250f4f512cc3a5d6dd0bd5940
anders kravis <anders@finimize.com>
2018-11-04 17:28:57 -0500

    basic page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'nuzzlebugs',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'nuzzlebugs',
+        short_name: 'nzl',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
